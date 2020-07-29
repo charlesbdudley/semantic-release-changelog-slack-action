3af371733e3738b4211ba90cdabba6fdbcad6677
Charles Dudley <cd@charlesdudley.com>
2020-07-29 17:34:27 -0500

    quick and dirty

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,4 +1,4 @@
-import {wait} from '../src/wait'
+import { wait } from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
@@ -18,10 +18,12 @@ test('wait 500 ms', async () => {
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
+  process.env['INPUT_SERVICE-NAME'] = ''
+  process.env['INPUT_SLACK-WEBHOOK-URL'] = ''
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecSyncOptions = {
     env: process.env
   }
+
   console.log(cp.execSync(`node ${ip}`, options).toString())
 })
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,119 @@
+import { promises as fs } from 'fs'
+import path from 'path'
 import * as core from '@actions/core'
-import {wait} from './wait'
+import { IncomingWebhook } from '@slack/webhook'
+import { Block } from '@slack/types'
+
+interface Section {
+  heading: string
+  text: string
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const webhookURL: string = core.getInput('slack-webhook-url')
+    const serviceName: string = core.getInput('service-name')
+    const changelog = await fs.readFile(
+      path.resolve(__dirname, '../CHANGELOG.md'),
+      {
+        encoding: 'utf-8'
+      }
+    )
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const chunkRegExp = /^#+ \[*\d+\.\d+\.\d+\]*/gm
+    const versionIndexes: number[] = []
+    let match
 
-    core.setOutput('time', new Date().toTimeString())
+    while ((match = chunkRegExp.exec(changelog)) !== null) {
+      versionIndexes.push(match.index)
+    }
+
+    const rawContent = changelog.slice(versionIndexes[0], versionIndexes[1])
+    const lines = rawContent.split('\n')
+    const versionLine = lines.shift()
+    const versionAndURLRegExp = /\[(\d+\.\d+\.\d+)\]\(([^)]+)/
+
+    if (!versionLine) {
+      return
+    }
+
+    const versionLineMatch = versionLine.match(versionAndURLRegExp)
+
+    if (!versionLineMatch) {
+      return
+    }
+
+    const [, version, versionURL] = versionLineMatch
+    const headingRegExp = /### (.+)/gm
+    const headingIndexes: number[] = []
+    const details = lines.join('\n')
+
+    while ((match = headingRegExp.exec(details)) !== null) {
+      headingIndexes.push(match.index)
+    }
+
+    const sections: Section[] = headingIndexes.reduce((sum, current, index) => {
+      const rawSection = details.slice(current, headingIndexes[index + 1])
+      const sectionLines = rawSection.split('\n')
+      const sectionHeading = sectionLines.shift() || ''
+      const commitsRaw = sectionLines.join('\n').trim()
+      const commitRegExp = /- \*\*([^:]+):\*\* ([^(]+) \(\[([^\]]+)\]\(([^)]+)\)/gs
+      const commits = []
+
+      while ((match = commitRegExp.exec(commitsRaw)) !== null) {
+        if (match) {
+          const [, type, message, hash, url] = match
+
+          commits.push({
+            type,
+            message,
+            hash,
+            url
+          })
+        }
+      }
+
+      const text = commits.reduce((textSum, commit) => {
+        return `${textSum}• *${commit.type}:* ${commit.message.replace(
+          /\n/g,
+          ''
+        )} (<${commit.url}|${commit.hash}>)\n`
+      }, '')
+
+      return [
+        ...sum,
+        {
+          heading: sectionHeading.replace('### ', ''),
+          text: text.trim()
+        }
+      ]
+    }, [] as Section[])
+
+    const webhook = new IncomingWebhook(webhookURL)
+
+    await webhook.send({
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `:rocket: New *${serviceName}* release <${versionURL}|v${version}>`
+          }
+        },
+        ...sections.reduce((sum, section) => {
+          return [
+            ...sum,
+            {
+              type: 'section',
+              text: {
+                type: 'mrkdwn',
+                text: `*${section.heading}*\n${section.text}`
+              }
+            }
+          ]
+        }, [] as Block[])
+      ]
+    })
   } catch (error) {
     core.setFailed(error.message)
   }
